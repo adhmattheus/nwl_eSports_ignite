dab06cfb744fd4763bd19c094f49ea5d7177642a
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-10-04 16:40:20 -0300

    criação das rotas do servidor

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,19 @@ import express from 'express'
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+//HTTP methods / API RESTful / HTTP Codes
+
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id; 
+
   return response.json([
     { id: 1, name: 'Anúncio 1' },
     { id: 3, name: 'Anúncio 2' },
@@ -10,5 +22,12 @@ app.get('/ads', (request, response) => {
     { id: 3, name: 'Anúncio 4' },
   ]);
 });
+
+app.get('/ads:id/discord', (request, response) => {
+  // const adId = request.params.id; 
+
+  return response.json([]);
+});
+
 
 app.listen(3333);
