11c38c39005800e83d5dcbe4e30d300cff5e61f7
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-10-10 20:12:46 -0300

    caixa de anúncios

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,11 +4,11 @@ import logoImg from './assets/Logo.svg'
 function App() {
 
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-10">
+    <div className="max-w-[1024px] mx-auto flex flex-col items-center my-10">
 
       <img src={logoImg} />
 
-      <h1 className='text-6xl text-white font-black mt-20'>
+      <h1 className='text-6xl text-white font-black mt-15'>
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
       </h1>
 
@@ -63,6 +63,21 @@ function App() {
         </a>
 
 
+      </div>
+
+      <div className='pt-1 bg-nlw-gradient self-stretch  mt-8 rounded-lg '>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou o seu duo?</strong>
+            <span className='text-zinc-400'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600  text-white rounded'>
+            Publica anúncio
+          </button>
+
+        </div>
       </div>
 
     </div>
