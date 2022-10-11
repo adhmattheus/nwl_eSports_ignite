9f06be80212620e75837631da4116cdcf51b5292
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-10-11 09:46:00 -0300

    banner createAds

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import './styles/main.css'
 import logoImg from './assets/Logo.svg'
 import { MagnifyingGlassPlus } from 'phosphor-react'
 import { GammerBanner } from './components/GamerBanner'
-
+import { CreateAdsBanner } from './components/CreateAdsBanner'
 
 function App() {
 
@@ -26,24 +26,9 @@ function App() {
 
       </div>
 
-      <div className='pt-1 bg-nlw-gradient self-stretch  mt-8 rounded-lg '>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-
-          <div>
-            <strong className='text-2xl text-white font-black block'>Não encontrou o seu duo?</strong>
-            <span className='text-zinc-400'>Publique um anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600  text-white rounded flex items-center gap-3'>
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-
-        </div>
-      </div>
+      <CreateAdsBanner />
 
     </div>
   )
 }
-
 export default App
